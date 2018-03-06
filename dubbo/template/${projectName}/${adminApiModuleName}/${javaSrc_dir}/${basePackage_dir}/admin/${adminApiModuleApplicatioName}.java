80f59ef09cc080ab5d8106a767f655f134cca179
xi <rarexixi@gmail.com>
2018-03-06 14:55:59 +0800

    模版修改

--- dubbo/template/${projectName}/${adminApiModuleName}/${javaSrc_dir}/${basePackage_dir}/admin/${adminApiModuleApplicatioName}.java
+++ dubbo/template/${projectName}/${adminApiModuleName}/${javaSrc_dir}/${basePackage_dir}/admin/${adminApiModuleApplicatioName}.java
@@ -1,12 +1,12 @@
-package ${basePackage};
+package ${basePackage}.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ${adminApiModuleApplicatioName} {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(${adminApiModuleApplicatioName}.class, args);
 	}
 }
