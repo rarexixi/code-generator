9867eb0e4e65a4658feef25d39f5e04661c7e913
xi <rarexixi@gmail.com>
2018-03-15 18:56:59 +0800

    删除废弃代码

--- spring-cloud/template/${projectName}/${adminModuleName}/${javaSrc_dir}/${basePackage_dir}/admin/${adminModuleApplicatioName}.java
+++ spring-cloud/template/${projectName}/${adminModuleName}/${javaSrc_dir}/${basePackage_dir}/admin/${adminModuleApplicatioName}.java
@@ -3,13 +3,9 @@ package ${basePackage}.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
 @EnableCircuitBreaker
-@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class ${adminModuleApplicatioName} {
--- spring-cloud/template/${projectName}/${providerModuleName}/${javaSrc_dir}/${basePackage_dir}/${providerModuleApplicationName}.java
+++ spring-cloud/template/${projectName}/${providerModuleName}/${javaSrc_dir}/${basePackage_dir}/${providerModuleApplicationName}.java
@@ -3,11 +3,7 @@ package ${basePackage};
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
 @EnableEurekaClient
 @SpringBootApplication
 public class ${providerModuleApplicationName} {
