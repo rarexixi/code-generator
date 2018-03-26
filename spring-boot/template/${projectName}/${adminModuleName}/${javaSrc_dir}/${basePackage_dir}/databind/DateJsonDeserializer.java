7fadc5be3a29fb975cfd552fa7ccdba5693b03ea
xi <rarexixi@gmail.com>
2018-03-26 16:37:01 +0800

    模版修改

--- spring-boot/template/${projectName}/${adminModuleName}/${javaSrc_dir}/${basePackage_dir}/databind/DateJsonDeserializer.java
+++ spring-boot/template/${projectName}/${adminModuleName}/${javaSrc_dir}/${basePackage_dir}/databind/DateJsonDeserializer.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import org.xi.common.utils.StringUtil;
+import ${baseCommonPackage}.utils.StringUtil;
 
 import java.io.IOException;
 import java.text.ParseException;
