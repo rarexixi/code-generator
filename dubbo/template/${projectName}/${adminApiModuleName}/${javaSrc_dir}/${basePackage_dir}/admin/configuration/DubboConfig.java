6db8175af7f5b1342c44db2d35c9f1036f7b3d7d
xi <rarexixi@gmail.com>
2018-05-07 15:35:03 +0800

    修改模版，修改dubbo自动配置

--- dubbo/template/${projectName}/${adminApiModuleName}/${javaSrc_dir}/${basePackage_dir}/admin/configuration/DubboConfig.java
+++ dubbo/template/${projectName}/${adminApiModuleName}/${javaSrc_dir}/${basePackage_dir}/admin/configuration/DubboConfig.java
@@ -1,65 +1,16 @@
 package ${basePackage}.admin.configuration;
 
-import ${baseCommonPackage}.model.dubbo.*;
-import ${basePackage}.admin.properties.DubboProperties;
-
-import com.alibaba.dubbo.config.ApplicationConfig;
-import com.alibaba.dubbo.config.ConsumerConfig;
-import com.alibaba.dubbo.config.ProtocolConfig;
-import com.alibaba.dubbo.config.RegistryConfig;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@ConditionalOnProperty(prefix = "dubbo", name = "enabled", matchIfMissing = true, havingValue = "true")
 @Configuration
 public class DubboConfig {
 
-    @Autowired
-    DubboProperties dubboProperties;
-
-    @Bean
-    public ApplicationConfig applicationConfig() {
-
-        DubboApplicationProperties applicationProperties = dubboProperties.getApplication();
-
-        ApplicationConfig applicationConfig = new ApplicationConfig();
-        applicationConfig.setId(applicationProperties.getId());
-        applicationConfig.setName(applicationProperties.getName());
-        return applicationConfig;
-    }
-
-    @Bean
-    public ProtocolConfig protocolConfig() {
-
-        DubboProtocolProperties protocolProperties = dubboProperties.getProtocol();
-
-        ProtocolConfig protocolConfig = new ProtocolConfig();
-        protocolConfig.setName(protocolProperties.getName());
-        protocolConfig.setPort(protocolProperties.getPort());
-        return protocolConfig;
-    }
-
-    @Bean
-    public ConsumerConfig consumerConfig() {
-
-        DubboConsumerProperties consumerProperties = dubboProperties.getConsumer();
-
-        ConsumerConfig consumerConfig = new ConsumerConfig();
-        consumerConfig.setTimeout(consumerProperties.getTimeout());
-        return consumerConfig;
-    }
-
-    @Bean
-    public RegistryConfig registryConfig() {
-
-        DubboRegistryProperties registryProperties = dubboProperties.getRegistry();
-
-        RegistryConfig registryConfig = new RegistryConfig();
-        registryConfig.setId(registryProperties.getId());
-        registryConfig.setProtocol(registryProperties.getProtocol());
-        registryConfig.setAddress(registryProperties.getAddress());
-        registryConfig.setClient(registryProperties.getClient());
-        return registryConfig;
+    @EnableDubboConfig
+    @EnableConfigurationProperties(DubboPropertiesConfig.class)
+    protected static class DubboPropertiesConfig {
     }
 }
