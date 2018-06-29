b60465c0e121d7b10ffc6ec50859452b9c8720d7
xi <rarexixi@gmail.com>
2018-06-29 13:10:45 +0800

    模版逻辑 跨域修改

--- spring-cloud/template/${projectName}/${adminApiModuleName}/${javaSrc_dir}/${basePackage_dir}/admin/configuration/CorsConfig.java
+++ spring-cloud/template/${projectName}/${adminApiModuleName}/${javaSrc_dir}/${basePackage_dir}/admin/configuration/CorsConfig.java
@@ -1,26 +1,44 @@
 package ${basePackage}.admin.configuration;
 
-import ${basePackage}.admin.filter.CorsFilter;
 import ${basePackage}.admin.properties.CorsProperties;
+import ${basePackage}.admin.properties.CorsRegistrationProperty;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Autowired
     CorsProperties corsProperties;
 
-    @Bean
-    public FilterRegistrationBean filterRegistrationBean(CorsFilter corsFilter) {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        registrationBean.setFilter(corsFilter);
-        if (corsProperties.getUrlPatterns() != null && !corsProperties.getUrlPatterns().isEmpty()) {
-            registrationBean.setUrlPatterns(corsProperties.getUrlPatterns());
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        CorsRegistrationProperty[] corsRegistrations = corsProperties.getCorsRegistrations();
+
+        if (corsRegistrations == null || corsRegistrations.length == 0) return;
+        for (CorsRegistrationProperty property : corsRegistrations) {
+            String pathPattern = property.getPathPattern();
+            if (pathPattern == null || pathPattern.isEmpty()) continue;
+            CorsRegistration corsRegistration = registry.addMapping(pathPattern);
+
+            if (property.getAllowedOrigins() != null)
+                corsRegistration.allowedOrigins(property.getAllowedOrigins());
+            if (property.getAllowedMethods() != null)
+                corsRegistration.allowedMethods(property.getAllowedMethods());
+            if (property.getAllowedHeaders() != null)
+                corsRegistration.allowedHeaders(property.getAllowedHeaders());
+            if (property.getExposedHeaders() != null)
+                corsRegistration.exposedHeaders(property.getExposedHeaders());
+            if (property.getAllowCredentials() != null)
+                corsRegistration.allowCredentials(property.getAllowCredentials());
+            if (property.getMaxAge() != null)
+                corsRegistration.maxAge(property.getMaxAge());
         }
-        return registrationBean;
     }
 }
--- spring-cloud/template/${projectName}/${adminApiModuleName}/${javaSrc_dir}/${basePackage_dir}/admin/properties/CorsProperties.java
+++ spring-cloud/template/${projectName}/${adminApiModuleName}/${javaSrc_dir}/${basePackage_dir}/admin/properties/CorsProperties.java
@@ -3,31 +3,18 @@ package ${basePackage}.admin.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
-
 @Component
-@ConfigurationProperties(prefix = "cors.response")
+@ConfigurationProperties(prefix = "cors")
 public class CorsProperties {
 
-    private Map<String, String> headers = new HashMap<>();
-    private Set<String> urlPatterns = new LinkedHashSet<>();
+    private CorsRegistrationProperty[] corsRegistrations;
 
-    public Map<String, String> getHeaders() {
-        return headers;
+    public CorsRegistrationProperty[] getCorsRegistrations() {
+        return corsRegistrations;
     }
 
-    public void setHeaders(Map<String, String> headers) {
-        this.headers = headers;
-    }
-
-    public Set<String> getUrlPatterns() {
-        return urlPatterns;
-    }
-
-    public void setUrlPatterns(Set<String> urlPatterns) {
-        this.urlPatterns = urlPatterns;
+    public void setCorsRegistrations(CorsRegistrationProperty[] corsRegistrations) {
+        this.corsRegistrations = corsRegistrations;
     }
 }
+
