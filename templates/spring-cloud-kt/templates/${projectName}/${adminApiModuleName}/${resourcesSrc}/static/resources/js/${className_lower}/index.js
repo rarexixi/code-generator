ba8299a03424f7d2b7bf3d8a14e7a0bbaacae27e
xishihao <xishihao@chehejia.com>
2019-01-12 17:48:03 +0800

    修改模板

--- templates/spring-cloud-kt/templates/${projectName}/${adminApiModuleName}/${resourcesSrc}/static/resources/js/${className_lower}/index.js
+++ templates/spring-cloud-kt/templates/${projectName}/${adminApiModuleName}/${resourcesSrc}/static/resources/js/${className_lower}/index.js
@@ -251,14 +251,14 @@ var app = new Vue({
         },
         <#if table.validStatusColumn??>
         enableSelected: function () {
-            this.execSelected("确定启用吗？", appConfig.baseApiPath + '/${classNameFirstLower}/enableList', "启用成功！", "启用失败！");
+            this.execSelected("确定启用吗？", appConfig.baseApiPath + '/${classNameFirstLower}/enable', "启用成功！", "启用失败！");
         },
         disableSelected: function () {
-            this.execSelected("确定禁用吗？", appConfig.baseApiPath + '/${classNameFirstLower}/disableList', "禁用成功！", "禁用失败！");
+            this.execSelected("确定禁用吗？", appConfig.baseApiPath + '/${classNameFirstLower}/disable', "禁用成功！", "禁用失败！");
         },
         </#if>
         delSelected: function () {
-            this.execSelected("确定删除吗？", appConfig.baseApiPath + '/${classNameFirstLower}/deleteList', "删除成功！", "删除失败！");
+            this.execSelected("确定删除吗？", appConfig.baseApiPath + '/${classNameFirstLower}/delete', "删除成功！", "删除失败！");
         },
         execSelected: function (confirmMsg, url, successMsg, failMsg) {
             var self = this;
@@ -267,12 +267,16 @@ var app = new Vue({
                 var item = self.multipleSelection[i];
                 checkedList.push(item.${table.uniPk.targetName?uncap_first});
             }
+
+            var params = {
+                ${table.uniPk.targetName?uncap_first}List: checkedList
+            };
             self.$confirm(confirmMsg, '', {
                 confirmButtonText: '确定',
                 cancelButtonText: '取消',
                 type: 'warning'
             }).then(function () {
-                self.ajaxPost(url, checkedList, failMsg, function(response) {
+                self.ajaxPost(url, params, failMsg, function(response) {
                     self.$notify({
                         type: 'success',
                         message: successMsg
